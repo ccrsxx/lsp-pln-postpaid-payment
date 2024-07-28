d633cc0e7cbf5c7e8491d3e9df575dd87b89393b
ccrsxx <aminrisal@gmail.com>
2024-07-28 22:18:40 +0700

    feat: finish project

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# Learnify Frontend
+# PLN Postpaid Payment
+
+## Tech Stack
+
+- Next.js
+- Auth.js
+- TypeScript
+- PostgreSQL
+- Tailwind CSS
+- Shadcn UI
+- Prisma
+- Zod
 
 ## Development
 
@@ -7,13 +18,13 @@ Here are the steps to run the project locally.
 1. Clone the repository
 
    ```bash
-   git clone https://github.com/ccrsxx/learnify-frontend.git
+   git clone https://github.com/ccrsxx/pln-postpaid-payment.git
    ```
 
 1. Change directory to the project
 
    ```bash
-   cd learnify-frontend
+   cd pln-postpaid-payment
    ```
 
 1. Install dependencies
@@ -22,9 +33,31 @@ Here are the steps to run the project locally.
    npm i
    ```
 
-1. Check your credentials on `.env.development`. Make sure you have the correct credentials. Or create `.env.local` so that it doesn't get tracked by git.
+1. Create a copy of the `.env.example` file and name it `.env.local`. Make sure to fill the credentials correctly.
 
-1. Run the project
+   ```bash
+   cp .env.example .env.local
+   ```
+
+1. Create DB if not exists
+
+   ```bash
+   npm run db:create
+   ```
+
+1. Run migrations
+
+   ```bash
+   npm run db:migrate
+   ```
+
+1. Run seeders
+
+   ```bash
+   npm run db:seed
+   ```
+
+1. Run the app
 
    ```bash
    npm run dev
--- src/components/dashboard/dashboard-sidebar.tsx
+++ src/components/dashboard/dashboard-sidebar.tsx
@@ -1,13 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import {
-  Users2,
-  History,
-  Package2,
-  Settings,
-  ShoppingCart
-} from 'lucide-react';
+import { Users2, History, Package2, ShoppingCart } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { useUser } from '@/lib/hooks/use-user';
@@ -22,7 +16,7 @@ import type { Role } from '@prisma/client';
 type SidebarItem = {
   href: string;
   role: Role;
-  Icon: typeof Settings;
+  Icon: typeof History;
   label: string;
 };
 
@@ -60,15 +54,15 @@ export function Sidebar(): JSX.Element {
 
   return (
     <aside className='fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex'>
-      <TooltipProvider>
-        <nav className='flex flex-col items-center gap-4 px-2 sm:py-5'>
-          <Link
-            href='#'
-            className='group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
-          >
-            <Package2 className='h-4 w-4 transition-all group-hover:scale-110' />
-            <span className='sr-only'>Acme Inc</span>
-          </Link>
+      <nav className='flex flex-col items-center gap-4 px-2 sm:py-5'>
+        <Link
+          href='#'
+          className='group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base'
+        >
+          <Package2 className='h-4 w-4 transition-all group-hover:scale-110' />
+          <span className='sr-only'>Acme Inc</span>
+        </Link>
+        <TooltipProvider>
           {sidebarItems.map(({ Icon, label, href, role }) => {
             const isShown = user?.role === role;
 
@@ -92,22 +86,8 @@ export function Sidebar(): JSX.Element {
               </Tooltip>
             );
           })}
-        </nav>
-        <nav className='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href='#'
-                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
-              >
-                <Settings className='h-5 w-5' />
-                <span className='sr-only'>Settings</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side='right'>Settings</TooltipContent>
-          </Tooltip>
-        </nav>
-      </TooltipProvider>
+        </TooltipProvider>
+      </nav>
     </aside>
   );
 }
